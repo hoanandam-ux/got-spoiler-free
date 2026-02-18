8d109362402c95875f56f1bd1310a398e54011dc
hoanandam-ux <hoanandam@gmail.com>
2026-02-18 16:04:10 +0700

    Update guard.js

--- guard.js
+++ guard.js
@@ -1,16 +1,2 @@
-const {
-  addHours,
-  isAfter,
-  isMonday,
-  isTuesday,
-  startOfDay,
-} = require('date-fns')
+module.exports = () => true
 
-module.exports = function guard (now) {
-  const limit = addHours(startOfDay(now), 3)
-
-  if (isMonday(now) && isAfter(now, limit)) return true
-  if (isTuesday(now) && isAfter(limit, now)) return true
-
-  return false
-}
