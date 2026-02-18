d1802a260f18695e1eef3e15ec9d38602b5955ca
hoanandam-ux <hoanandam@gmail.com>
2026-02-18 16:22:38 +0700

    Update orm.js

--- orm.js
+++ orm.js
@@ -1,29 +1,32 @@
-const db = require('level')('./users')
+const { Level } = require('level')
 
-const set = module.exports.set = (name, value) => {
-  return db.put(name, value)
+// tạo database ./users (value dạng string)
+const db = new Level('./users', { valueEncoding: 'utf8' })
+
+const set = module.exports.set = async (name, value) => {
+  await db.put(name, value)
 }
 
-const get = module.exports.get = (name, fallback = '') => {
-  return new Promise((resolve) => {
-    db.get(name, (err, value) => {
-      return err ? resolve(fallback) : resolve(value)
-    })
-  })
+const get = module.exports.get = async (name, fallback = 0) => {
+  try {
+    const value = await db.get(name)
+    return value
+  } catch (err) {
+    return fallback
+  }
 }
 
 const addUser = module.exports.addUser = async (name) => {
   const payload = await get('users', '')
   const users = payload ? payload.split(',') : []
   const uniq = [...new Set([...users, name])]
-
-  set('users', uniq.join())
+  await set('users', uniq.join(','))
 }
 
 const getUsers = module.exports.getUsers = async () => {
   const payload = await get('users', '')
   const users = payload ? payload.split(',').filter(x => x) : []
-  const warns = await Promise.all(users.map((user) => get(user, 0)))
 
+  const warns = await Promise.all(users.map((user) => get(user)))
   return users.map((user, index) => [user, warns[index]])
 }
